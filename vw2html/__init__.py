808b7d69760a59728925e5e9823f07d0f8106049
gryf <gryf73@gmail.com>
2024-04-20 17:13:49 +0200

    Removed random code from init.

--- vw2html/__init__.py
+++ vw2html/__init__.py
@@ -1,11 +1,1 @@
 __version__ = "0.0.1"
-
-from vw2html.parser import Parser
-
-
-def wiki(text_lines):
-    """
-    Converts vimwki input, return converted html string.
-    """
-    par = Parser()
-    return par.parse(text_lines)
